14045c559090ed54eb70aed189bb373b8771397e
Sam Maton <sam.maton91@gmail.com>
2025-10-16 22:58:28 +0100

    setup signup form

--- apps/frontend/src/app/routes/home/home.component.ts
+++ apps/frontend/src/app/routes/home/home.component.ts
@@ -1,6 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,12 +22,36 @@ import { MatButtonModule } from '@angular/material/button';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  showSignup = signal(false);
+  signUpPassword = signal('');
+
   loginForm = new FormGroup({
-    email: new FormControl(''),
+    email: new FormControl('', [Validators.email]),
     password: new FormControl(''),
   });
 
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.email]),
+    password: new FormControl('', [Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [
+      Validators.minLength(8),
+      Validators.pattern(this.signUpPassword()),
+    ]),
+  });
+
   onSubmit() {
-    console.log(this.loginForm.value);
+    console.log(this.loginForm);
+  }
+
+  onSignup() {
+    console.log(this.signupForm.value);
+  }
+
+  toggleSignup() {
+    this.showSignup.update((value) => !value);
+  }
+
+  updateSignUpPassword(event: Event) {
+    this.signUpPassword.set((event.target as HTMLInputElement).value);
   }
 }

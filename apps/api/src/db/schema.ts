1294a653ce3bfd847193a550e686b3bd410bffe3
Sam Maton <sam.maton91@gmail.com>
2025-10-16 11:19:08 +0100

    setup drizzle

---
+++ apps/api/src/db/schema.ts
@@ -0,0 +1,7 @@
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
+
+export const posts = sqliteTable('posts', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+});

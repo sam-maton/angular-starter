3956afa0274f6e104c72bcd83b0bf9b8ca40eb19
Sam Maton <sam.maton91@gmail.com>
2025-10-17 15:01:40 +0100

    finished setting up auth

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '../db/database.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, PostsModule],
+  imports: [DatabaseModule, PostsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db/client';
+import { account, session, user, verification } from '../db/schema';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: 'sqlite', // or "mysql", "sqlite"
+    provider: 'sqlite', // or "mysql", "sqlite",
+    schema: {
+      user,
+      account,
+      session,
+      verification,
+    },
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });

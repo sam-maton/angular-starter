0b867b1dae9544446363b6ad530499879c34e4ff
Sam Maton <sam.maton91@gmail.com>
2025-10-25 22:48:01 +0100

    added app bar shell

---
+++ apps/frontend/src/app/components/app-bar/app-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  imports: [],
+  templateUrl: './app-bar.component.html',
+  styleUrl: './app-bar.component.scss',
+})
+export class AppBarComponent {}
--- apps/frontend/src/app/routes/home/home.component.ts
+++ apps/frontend/src/app/routes/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppBarComponent } from '../../components/app-bar/app-bar.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [RouterModule],
+  imports: [RouterModule, AppBarComponent],
 })
 export class HomeComponent {}

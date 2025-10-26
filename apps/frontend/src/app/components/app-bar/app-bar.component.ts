24077b8417c97f95da11cfaa5c1b746cd23e59dc
Sam Maton <sam.maton91@gmail.com>
2025-10-26 21:15:37 +0000

    added icons and app bar

--- apps/frontend/src/app/components/app-bar/app-bar.component.ts
+++ apps/frontend/src/app/components/app-bar/app-bar.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-bar',
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './app-bar.component.html',
   styleUrl: './app-bar.component.scss',
 })
-export class AppBarComponent {}
+export class AppBarComponent implements OnInit {
+  menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Components',
+            icon: 'pi pi-bolt',
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+    ];
+  }
+}
